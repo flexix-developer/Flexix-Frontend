774230ee17aa8fa75466ecee6e05382ad56fb967
Mon Aekarin <Aekarin.Ongart@gmail.com>
2023-12-06 03:22:35 +0700

    Mon | Routes | Add Route path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import HomePage from "./pages/Home-Page";
-function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
-}
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/Home-Page';
+import LoginPage from './pages/Login-Page';
+import TutorialPage from './pages/Tutorial-Page';
+
+const App = () => {
+ return (
+    <>
+       <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/tutorial" element={<TutorialPage />} />
+       </Routes>
+    </>
+ );
+};
 
 export default App;
