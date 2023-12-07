a6f9194f9b35f9206b846d4b58862f23e2a0fedd
UnnopThamtiengtham <unnop1.tham@gmail.com>
2023-12-07 17:47:12 +0700

    nop | route platform

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginPage from "./pages/Login-Page";
 import TutorialPage from "./pages/Tutorial-Page";
 import ForgotPasswordPage from "./pages/ForgotPassword-Page";
 import RegisterPage from "./pages/Register-Page";
+import PlatformPage from "./pages/Platform-Page";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/tutorial" element={<TutorialPage />} />
         <Route path="/forgot" element={<ForgotPasswordPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/platform" element={<PlatformPage />} />
       </Routes>
     </>
   );
