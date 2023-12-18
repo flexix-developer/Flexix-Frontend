943b746699b711561173ea9558fb608b2b66c4cf
Mon Aekarin <Aekarin.Ongart@gmail.com>
2023-12-18 09:00:58 +0700

    Mon | Add Properties component

---
+++ src/components/Properties.js
@@ -0,0 +1,9 @@
+const Properties = () => {
+    return (
+        <div className="flex flex-col w-full h-full bg-neutral-700">
+            TEST
+        </div>
+    )
+};
+
+export default Properties;
