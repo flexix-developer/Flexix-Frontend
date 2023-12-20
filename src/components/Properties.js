9d49451d694f992484a5e9dd4ea8fa1674190ffb
Mon Aekarin <Aekarin.Ongart@gmail.com>
2023-12-20 12:52:22 +0700

    Mon | Refactor Properties component to handle different component selections

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,24 +1,56 @@
 import PropertiesStyle from "./PropertiesStyle";
+import PropertiesOptions from "./PropertiesOptions";
+import PropertiesData from "./PropertiesData";
+import { useState } from "react";
 
 const Properties = () => {
-    return (
-        <div className="flex flex-col w-full h-full bg-neutral-700">
-            <div className="flex flex-row justify-center rounded-2xl border-2 border-neutral-600 mx-8 my-2 bg-neutral-800 text-white w-12/12">
-                <div className="flex flex-row w-4/12 justify-center p-1 cursor-pointer">
-                    <p className="text-center">Options</p>
-                </div>
-                <div className="flex flex-row w-3/12 justify-center p-1 cursor-pointer">
-                    <p className="text-center">Style</p>
-                </div> 
-                <div className="flex flex-row w-3/12 justify-center p-1 cursor-pointer">
-                    <p className="text-center">Data</p>
-                </div>
-            </div>
-            <div>
-                <PropertiesStyle />
-            </div>
+  const [selectedComponent, setSelectedComponent] = useState("PropertiesStyle");
+
+  const handleComponentClick = (component) => {
+    setSelectedComponent(component);
+  };
+
+  return (
+    <div className="flex flex-col w-full h-full bg-neutral-700">
+      <div className="flex flex-row justify-center mx-6 my-2 text-white w-12/12">
+        <div
+          className={`flex flex-row w-3/12 justify-center p-1 border-2 border-y-neutral-500 border-l-neutral-500 border-r-neutral-800  rounded-l-xl bg-neutral-800 cursor-pointer ${
+            selectedComponent === "PropertiesOptions"
+              ? "bg-blue-500 text-black"
+              : ""
+          }`}
+          onClick={() => handleComponentClick("PropertiesOptions")}
+        >
+          <p className="text-center pl-1">Options</p>
         </div>
-    );
+        <div
+          className={`flex flex-row w-3/12 justify-center p-1 cursor-pointer border-2 border-y-neutral-500 border-x-neutral-800 bg-neutral-800 ${
+            selectedComponent === "PropertiesStyle"
+              ? "bg-blue-500 text-black"
+              : ""
+          }`}
+          onClick={() => handleComponentClick("PropertiesStyle")}
+        >
+          <p className="text-center">Style</p>
+        </div>
+        <div
+          className={`flex flex-row w-3/12 justify-center p-1 border-2 border-y-neutral-500 border-r-neutral-500 border-l-neutral-800 rounded-r-xl cursor-pointer bg-neutral-800 ${
+            selectedComponent === "PropertiesData"
+              ? "bg-blue-500 text-black"
+              : ""
+          }`}
+          onClick={() => handleComponentClick("PropertiesData")}
+        >
+          <p className="text-center">Data</p>
+        </div>
+      </div>
+      <div>
+        {selectedComponent === "PropertiesOptions" && <PropertiesOptions />}
+        {selectedComponent === "PropertiesStyle" && <PropertiesStyle />}
+        {selectedComponent === "PropertiesData" && <PropertiesData />}
+      </div>
+    </div>
+  );
 };
 
 export default Properties;
