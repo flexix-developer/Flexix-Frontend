2e6344123cfa19c795309ae88144b76e3269f262
Mon Aekarin <Aekarin.Ongart@gmail.com>
2023-12-20 15:21:43 +0700

    Update Properties component styling

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -16,7 +16,7 @@ const Properties = () => {
         <div
           className={`flex flex-row w-3/12 justify-center p-1 border-2 border-y-neutral-500 border-l-neutral-500 border-r-neutral-800  rounded-l-xl bg-neutral-800 cursor-pointer ${
             selectedComponent === "PropertiesOptions"
-              ? "bg-blue-400 text-black"
+              ? "bg-blue-500 text-black"
               : ""
           }`}
           onClick={() => handleComponentClick("PropertiesOptions")}
@@ -26,7 +26,7 @@ const Properties = () => {
         <div
           className={`flex flex-row w-3/12 justify-center p-1 cursor-pointer border-2 border-y-neutral-500 border-x-neutral-800 bg-neutral-800 ${
             selectedComponent === "PropertiesStyle"
-              ? "bg-blue-400 text-black"
+              ? "bg-blue-500 text-black"
               : ""
           }`}
           onClick={() => handleComponentClick("PropertiesStyle")}
@@ -36,7 +36,7 @@ const Properties = () => {
         <div
           className={`flex flex-row w-3/12 justify-center p-1 border-2 border-y-neutral-500 border-r-neutral-500 border-l-neutral-800 rounded-r-xl cursor-pointer bg-neutral-800 ${
             selectedComponent === "PropertiesData"
-              ? "bg-blue-400 text-black"
+              ? "bg-blue-500 text-black"
               : ""
           }`}
           onClick={() => handleComponentClick("PropertiesData")}
