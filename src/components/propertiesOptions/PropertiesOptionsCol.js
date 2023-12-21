64e0dfb28af744018b9489f8ea00d714007db5ce
Mon Aekarin <Aekarin.Ongart@gmail.com>
2023-12-21 14:46:55 +0700

    Mon | Add link URL input field to PropertiesOptionsCol.js

--- src/components/propertiesOptions/PropertiesOptionsCol.js
+++ src/components/propertiesOptions/PropertiesOptionsCol.js
@@ -66,6 +66,19 @@ const PropertiesOptionsCol = () => {
           </div>
         </div>
       </div>
+      <div className="flex flex-row pl-2 pb-1 text-lg mt-2">
+        <p>Link URL</p>
+      </div>
+      <div className="flex flex-row pl-2 pb-1">
+        <input
+          type="text"
+          placeholder="e.g. https://www.google.com"
+          className="w-11/12 p-1 pl-2 bg-neutral-700 rounded border-2 border-neutral-600"
+        />
+      </div>
+      <div className="flex flex-row pl-2 pb-1 text-xs w-full text-neutral-400">
+        <p>Set this to make this whole block link somewhere</p>
+      </div>
     </div>
   );
 };
