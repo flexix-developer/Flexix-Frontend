7dc85d8c35d6b3ecfb2d487ec299ab1c2b3552b1
Mon Aekarin <Aekarin.Ongart@gmail.com>
2023-12-21 14:17:44 +0700

    Mon | Add block option toggle functionality

--- src/components/propertiesOptions/PropertiesOptions.js
+++ src/components/propertiesOptions/PropertiesOptions.js
@@ -1,8 +1,38 @@
+import PropertiesOptionsRow from "./PropertiesOptionsRow";
+import PropertiesOptionsCol from "./PropertiesOptionsCol";
+import WidgetsTitle from "../toolboxWidgets/ToolboxWidgetsTitle";
+import { useState } from "react";
+
 const PropertiesOptions = () => {
 
+    const [isBlockOptionVisible, setBlockOptionVisible] = useState(true);
+
+    const [selectedComponent] = useState("PropertiesOptionsCol");
+    
+    // const [selectedComponent, setSelectedComponent] = useState("PropertiesOptionsCol");
+    // const handleComponentClick = (component) => {
+    //   setSelectedComponent(component);
+    // };
+
+    const handleWidgetsToggle = (widgetType, isOpen) => {
+        switch (widgetType) {
+          case "Block Option":
+            setBlockOptionVisible(isOpen);
+            break;
+          default:
+        break;
+    }
+  };
+
     return (
-        <div className="flex flex-col text-white items-center mt-2">
-            <h1>PropertiesOptions is comming...</h1>
+        <div className="flex flex-col bg-neutral-700 text-white">
+            <WidgetsTitle
+            title="Block Option"
+            onToggle={(isOpen) => handleWidgetsToggle("Block Option", isOpen)}
+            />
+            {isBlockOptionVisible && 
+            ((selectedComponent === "PropertiesOptionsRow" && <PropertiesOptionsRow />) 
+            || (selectedComponent === "PropertiesOptionsCol" && <PropertiesOptionsCol />)) }
         </div>
     )
 
