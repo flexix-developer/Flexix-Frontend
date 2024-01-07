0ec9c09a083dfdba0d53777a6033e5b54869c948
Mon Aekarin <Aekarin.Ongart@gmail.com>
2024-01-08 00:30:40 +0700

    Mon | Refactor Designs-Page.js component

--- src/pages/Designs-Page.js
+++ src/pages/Designs-Page.js
@@ -21,8 +21,7 @@ const DesignPage = () => {
   const handleLayerClick = (layer) => setSelectedLayer(layer);
 
   const handleCreateButtonClick = () => {
-    
-    const newHTMLCode = "Your generated HTML code"; 
+    const newHTMLCode = "Your generated HTML code";
 
     setCode(newHTMLCode);
     console.log("New HTML code:", newHTMLCode);
@@ -184,23 +183,15 @@ const DesignPage = () => {
       {showModal && (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-2xl">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-900 text-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Create new page</h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
+                <div className="flex items-start justify-start p-6 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-4xl font-semibold">Create new page</h3>
                 </div>
                 {/*body*/}
-                <div className="relative p-6 flex-auto">
+                <div className="relative p-6 flex-auto text-xl">
                   <label>Page name:</label>
                   <input
                     type="text"
@@ -220,7 +211,7 @@ const DesignPage = () => {
                     className="bg-gray-100 p-2 mb-4 mt-1 rounded-md focus:outline-none focus:border-blue-500 w-full text-black w-full"
                   />
                   <div className="my-2">
-                  <label
+                    <label
                       className="inline-block pr-2 hover:cursor-pointer"
                       htmlFor="flexSwitchCheckDefault"
                     >
